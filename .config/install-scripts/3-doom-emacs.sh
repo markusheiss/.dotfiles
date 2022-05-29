656963cc84f8fc2159fed9d3633db3bed89e59a9
Markus <markus@debian>
2022-05-29 16:41:28 +0200

    modifed stow install

--- .config/install-scripts/3-doom-emacs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ~/.emacs.d
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-~/.emacs.d/bin/doom install
---
+++ .config/install-scripts/3-npm-packages.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo npm install -g \
+	live-server \
+	js-beautify \
+	stylelint \
+	marked
