d14cff0674c73234161d6f29b484ae463005edff
Markus <70845953+markusheiss@users.noreply.github.com>
2022-07-04 19:37:21 +0200

    Update 6-oh-my-zsh.sh

--- .config/install-scripts/6-oh-my-zsh.sh
+++ .config/install-scripts/6-oh-my-zsh.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-
-# PLUGINS
+# Download plugins first
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
